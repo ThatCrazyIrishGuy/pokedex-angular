89b075dcd4ad1337dfae3cab004a1d71bde8e8c1
Darren Britton <darrenbritton@hotmail.com>
2016-02-18 18:00:47 +0000

    boostraped and added move loaded and extra stats

--- app.js
+++ app.js
@@ -4,18 +4,25 @@ myApp.controller('PokedexController',
     function PokedexController($scope, $http) {
         $scope.details = {};
         $scope.change = function() {
-            $http.get("http://pokeapi.co/api/v1/pokemon/" + $scope.search + '/')
+            $http.get("http://pokeapi.co/api/v1/pokemon/" + $scope.search.toLowerCase() + '/')
                 .success(function(pokemon) {
-                    //console.log(pokemon);
+                    console.log(pokemon);
                     $scope.details = pokemon;
                     $scope.related = pokemon.evolutions;
+                    $scope.details.expandedMoves = [];
                     //console.log($scope.related);
                     $scope.details.sprite = 'http://pokeapi.co/media/img/' + pokemon.national_id + '.png'
-                    $http.get("http://pokeapi.co" + pokemon.descriptions[0].resource_uri)
+                    $http.get("http://pokeapi.co" + pokemon.descriptions[pokemon.descriptions.length - 1].resource_uri)
                         .success(function(detail) {
                             //console.log(detail);
                             $scope.details.description = detail.description;
                         });
+                    pokemon.moves.forEach(function(move) {
+                        $http.get("http://pokeapi.co" + move.resource_uri)
+                            .success(function(detail) {
+                                $scope.details.expandedMoves.push(detail);
+                            });
+                    });
                 });
         }
         $scope.update = function(pokemon) {
